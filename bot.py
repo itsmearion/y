a2f59a4a8fa0e432c12456b302c5c6faf62f22e8
itsmearion <bilqiss0893@gmail.com>
2025-04-24 15:02:49 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,9 +5,9 @@ from handlers import personal_command, call_command, unpersonal_command
 
 app = Client(
     "mybot",
-    api_id=config.api_id,
-    api_hash=config.api_hash,
-    bot_token=config.bot_token
+    api_id=config.API_ID,
+    api_hash=config.API_HASH,
+    bot_token=config.BOT_TOKEN
 )
 
 # Daftarkan semua handler
