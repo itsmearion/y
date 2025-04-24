dbb5b1c805f9c03e4df6cb42b91a48c3494fec7f
itsmearion <bilqiss0893@gmail.com>
2025-04-24 14:32:42 +0700

    Create call_command.py

---
+++ handlers/call_command.py
@@ -0,0 +1,21 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+import json
+import os
+
+DB_FILE = "storage.json"
+
+def load_commands():
+    if not os.path.exists(DB_FILE):
+        return {}
+    with open(DB_FILE, "r") as f:
+        return json.load(f)
+
+def add_handlers(app: Client):
+
+    @app.on_message(filters.command(None) & filters.private)
+    async def call_command(_, msg: Message):
+        data = load_commands()
+        cmd = msg.text[1:].split()[0].lower()
+        if cmd in data:
+            await msg.reply(data[cmd])
