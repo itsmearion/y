f36139357a3d9dadfd88d15c34a1b09d0d0a9a46
itsmearion <bilqiss0893@gmail.com>
2025-04-24 15:12:08 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,18 +1,9 @@
 from pyrogram import Client
-import config  # import dari config.py
+from config import API_ID, API_HASH, BOT_TOKEN
+from handlers import call_command
 
-from handlers import personal_command, call_command, unpersonal_command
+app = Client("mybot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
-app = Client(
-    "mybot",
-    api_id=config.API_ID,
-    api_hash=config.API_HASH,
-    bot_token=config.BOT_TOKEN
-)
-
-# Daftarkan semua handler
-personal_command.add_handlers(app)
-unpersonal_command.add_handlers(app)
 call_command.add_handlers(app)
 
 app.run()
