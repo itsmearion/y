e2f9ca0453c8cb5c68322bce8759df8ac5bdf1b7
itsmearion <bilqiss0893@gmail.com>
2025-04-24 14:39:12 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 API_ID = 24488567        # Ganti dengan API ID dari my.telegram.org
 API_HASH = "51bb44c94b468bd8955f9e2916ed1402"  # Ganti dengan API Hash
-BOT_TOKEN = "7674931376:AAHILgMWDqLsV-eKVJz4ARgETCIxC5B_Yi8"  
+BOT_TOKEN = "7674931376:AAENUEbxz12Zm_vEAE8MtHCrSrHYBsE2aHU"  
